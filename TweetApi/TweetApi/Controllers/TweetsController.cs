5450e54ad5a566379908465f5625fef3912406ba
Ashwarya Joshi <ajoshi@tavisca.com>
2018-09-29 17:11:29 +0530

    Web api made performing get post and delete operations

---
+++ TweetApi/TweetApi/Controllers/TweetsController.cs
@@ -0,0 +1,53 @@
+﻿using Aerospike.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TweetApi.Models;
+
+namespace TweetApi.Controllers
+{
+    public class TweetsController : ApiController
+    {
+        AerospikeClient aerospikeClient = new AerospikeClient("18.235.70.103", 3000);
+        string nameSpace = "AirEngine";
+        string setName = "Ashwarya";
+        [HttpPost]
+        public List<DataRecords> GetAllRecords([FromBody] List<long> url)
+        {
+           
+            List<DataRecords> records = new List<DataRecords>();
+            foreach (long uid in url)
+            {
+                Record record = aerospikeClient.Get(new BatchPolicy(), new Key(nameSpace, setName, uid.ToString()));
+                DataRecords result = new DataRecords();
+                
+                result.author = record.GetValue("author").ToString();
+                result.author_id = record.GetValue("authorid").ToString();
+                result.following = int.Parse(record.GetValue("following").ToString());
+                result.followers = int.Parse(record.GetValue("followers").ToString());
+                result.content = record.GetValue("content").ToString();
+                result.region = record.GetValue("region").ToString();
+                result.language = record.GetValue("language").ToString();
+                result.tweet_id = record.GetValue("id").ToString();
+                result.retweet = int.Parse(record.GetValue("retweet").ToString());
+               
+                records.Add(result);
+            }
+            return records;
+
+        }
+  
+        public void Put([FromBody]DataRecords dataRecords)
+        {
+            aerospikeClient.Put(new WritePolicy(), new Key(nameSpace, setName, dataRecords.tweet_id), new Bin[] { new Bin("content", dataRecords.content) });
+        }
+        public void Delete([FromBody]string url)
+        {
+            aerospikeClient.Delete(new WritePolicy(), new Key(nameSpace, setName, url));
+        }
+
+    }
+}
---
+++ TweetApi/TweetApi/Models/DataRecords.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TweetApi.Models
+{
+    public class DataRecords
+    {
+        public string author { get; set; }
+        public string content { get; set; }
+        public string region { get; set; }
+        public string language { get; set; }
+        public int following { get; set; }
+        public int followers { get; set; }
+        public string post_url { get; set; }
+        public int retweet { get; set; }
+        public string tweet_id { get; set; }
+        public string author_id { get; set; }
+    }
+}
